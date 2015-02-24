745415a3081c82b8aa154ef5e5faba257d9dd7c6
DanieleMaroneseNTT <daniele.maronese@nttdata.com>
2015-02-24 16:49:25 +0100

    Primo commit

---
+++ infocar/.sencha_backup/Infocar/2.4.0.487/resources/js/local-it.js
@@ -0,0 +1,15 @@
+Ext.Date.monthNames = [
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre'
+];
+
